0a6f111314329d3fb529fb3ef9a780976a8c772b
Kaif Shaikh Siddique <85302180+kaif21-cmd@users.noreply.github.com>
2024-05-19 21:29:39 +0530

    Add files via upload

---
+++ DAY1.JS/Q2.js
@@ -0,0 +1,5 @@
+console.log((true + "")[3]); // 'e'
+//explanation any datatype when add with string it always concatinate so it look like "true"[3] and index of 3 is e so e is the ans 
+console.log((true+"kaif"[3]))// 'f'
+console.log((false+"zara"[2]))//'r'
+
