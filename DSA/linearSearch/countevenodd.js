c0d9ccfe35f0744d4e4391b6e75fdd9afb5e3f85
Kaif Shaikh Siddique <85302180+kaif21-cmd@users.noreply.github.com>
2026-02-10 17:38:19 +0530

    Add files via upload

---
+++ DSA/linearSearch/countevenodd.js
@@ -0,0 +1,29 @@
+function count_even_odd(ar) {
+  let counteven = 0;
+  let countodd = 0;
+
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i] % 2 === 0) {
+      counteven++;
+    } else {
+      countodd++;
+    }
+  }
+
+  return { even: counteven, odd: countodd };
+}
+
+let result = count_even_odd([1, 2, 3, 4, 5, 6, 7, 8]);
+// console.log(result);
+
+function evenoddidx(ar){
+    let idx=[]
+    for(let i=0;i<ar.length;i++){
+        if(ar[i]%2===0){
+         idx.push(i)
+        }
+    }
+    return idx;
+}
+let result2=evenoddidx([1,2,3,4,5,6])
+console.log(result2)
